4e78d4487d5bfda7a0ba7b1934bc691ce3bd38c4
Pushkar <thepsguy@icloud.com>
2016-02-19 19:12:55 +0530

    Initial Commit

---
+++ UzitNow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UzitNow
+//
+//  Created by Pushkar Sharma on 19/02/16.
+//  Copyright © 2016 thePsguy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UzitNow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UzitNow
+//
+//  Created by Pushkar Sharma on 19/02/16.
+//  Copyright © 2016 thePsguy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
